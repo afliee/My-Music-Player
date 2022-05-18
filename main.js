097dfdbb9e0cdd56dd25b15a7eff2a859869327f
Kunzzz <95630575+marcusKunn-3108@users.noreply.github.com>
2022-05-18 15:18:49 +0700

    change

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const cd = $('.cd')
 const player = $('.player')
 const playBtn = $('.btn-toggle-play')
 const nextBtn = $('.btn-next')
+const progress = $('#progress')
 const app = {
     currentIndex: 0,
     isPlaying: false,
@@ -43,7 +44,7 @@ const app = {
         {
           name: "Phi Hành Gia",
           singer: "Raftaar",
-          path: "https://ac.zingmp3.vn/v1/web/suggestion-keywords?num=10&query=phi%20h%C3%A0nh%20gia&language=vi&ctime=1652849097&version=1.6.25&sig=b0ad21bcb86e6de1a8ec7c061917d20ce223f3dc6b6da91dd84c0f58a8df3b6c9e9d94e36be32ff0e3e9cf8c7fc9111dd18e5e034ae21cae892ed99272cb31aa&apiKey=88265e23d4284f25963e6eedac8fbfa3",
+          path: "https://mp3-s1-zmp3.zmdcdn.me/136884ef90ae79f020bf/1641773109473754030?authen=exp=1653030356~acl=/136884ef90ae79f020bf/*~hmac=557b01813ba71f01c8c54e8fe4932458&fs=MTY1Mjg1NzU1NjY2Mnx3ZWJWNnwwfDM2LjM3LjE3OS4xOTU",
           image:
             "https://photo-resize-zmp3.zmdcdn.me/w165_r1x1_jpeg/cover/8/6/d/6/86d68a321a1b7562b1bbf8e37642343a.jpg"
         },
@@ -57,7 +58,7 @@ const app = {
         {
           name: "Sao Cha Không",
           singer: "Phan Mạnh Quỳnh",
-          path: "https://mp3-s1-zmp3.zmdcdn.me/2d287496bfd2568c0fc3/9141546019302212699?authen=exp=1653022168~acl=/2d287496bfd2568c0fc3/*~hmac=af20753f214ecbb31667726282866485&fs=MTY1Mjg0OTM2ODmUsICzM3x3ZWJWNnwxMDmUsICzNDmUsICwMzE0fDIyMi4yNTMdUngMTI1LjU3",
+          path: "https://mp3-s1-zmp3.zmdcdn.me/f24ae75bce1d27437e0c/2224528644319819820?authen=exp=1653031228~acl=/f24ae75bce1d27437e0c/*~hmac=5de3f5b6e3813c6db46dccccc751e67e&fs=MTY1Mjg1ODQyODY4NHx3ZWJWNnwxMDmUsICzNDmUsICwMzE0fDExNS43OC4yMTEdUngOTY",
           image:
             "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/f/3/1/e/f31efb0da9bc984d7246866e6d529d78.jpg"
         },
@@ -93,6 +94,14 @@ const app = {
         cd.style.width = newWidth > 0 ? newWidth + 'px' : 0
         cd.style.opacity = newWidth / cdWidth
       }
+
+      const cdThumbAnimate = cdThumb.animate([
+        {transform:'rotate(360deg)'}
+      ], {
+        duration: 10000,
+        iterations: Infinity
+      })
+      cdThumbAnimate.pause()
       // play music when click button is clicked
       playBtn.onclick = function() {
         if (_this.isPlaying) {
@@ -106,19 +115,39 @@ const app = {
       audio.onplay = function() {
         _this.isPlaying = true
         player.classList.add('playing')
+        cdThumbAnimate.play()
       }
 
       audio.onpause = function() {
         _this.isPlaying = false
         player.classList.remove('playing')
+        cdThumbAnimate.pause()
       }
 
       nextBtn.onclick = function() {
-        _this.currentIndex++
-        _this.loadCurrentSong()
-        audio.play()
-        
+        if (_this.currentIndex  !== _this.songs.length ) {
+          _this.loadCurrentSong()
+          audio.play()
+          _this.currentIndex++
+        }else {
+          _this.currentIndex = 0
+        }
       }
+
+      audio.ontimeupdate = function() {
+        if (audio.duration) {
+          const progressPercent = Math.floor(audio.currentTime / audio.duration * 100)
+          progress.value = progressPercent
+        }
+      }
+
+      progress.onchange = function(e) {
+        if (audio.duration){
+          audio.currentTime = e.target.value /100 * audio.duration
+          audio.play()
+        }
+      }
+
     },
     loadCurrentSong : function() {
       heading.textContent = this.currentSong.name
